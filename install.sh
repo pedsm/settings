265b4bc1e120d9d40bde4acac2be53ee37c09aea
Pedro Mendonca <peumendonca@gmail.com>
2016-11-23 06:46:14 +0000

    Added update and tmux conf as well as a bunch of script

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo Installing pedsm Settings
-echo installing zsh and oh-my-zsh
-sudo apt install zsh
+echo installing zsh and oh-my-zsh and tmux
+sudo apt update
+sudo apt install zsh tmux htop
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo Copying setting files
-cp -r .vim ~/.vim
-cp -r .vimrc ~/.vimrc
-cp -r .zshrc ~/.zshrc
+sudo cp -r .vim ~/.vim
+sudo cp -r .vimrc ~/.vimrc
+sudo cp -r .zshrc ~/.zshrc
+sudo cp -r .tmux.conf ~/.tmux.conf
 echo Installing vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-./.vim/bundle/YouCompleteMe/install.py
+~/.vim/bundle/YouCompleteMe/install.py
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Updating repo
+sudo cp -r ~/.vim .vim
+sudo cp ~/.vimrc .vimrc
+sudo cp ~/.zshrc .zshrc
+sudo cp ~/.tmux.conf .tmux.conf
+
